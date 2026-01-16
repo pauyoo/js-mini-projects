6a572d45290fa368a8eb4c483da6bea3313da1d6
Pauyoo <paulayodele04@gmail.com>
2026-01-16 01:23:27 +0100

    Counter Program

--- index 2.js
+++
@@ -1,8 +0,0 @@
-let fullname = "Atoyebi Paul";
-let newage = 25;
-let student = false;
-
-document.getElementById("p1").textContent = `Your name is ${fullname}`;
-document.getElementById("p1").textContent = `You are ${newage}`;
-document.getElementById("p1").textContent = `Enrolled: ${student}`;
-
