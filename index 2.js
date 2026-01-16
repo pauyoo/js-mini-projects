ae0456605734ddbf6302a763b0ffa74df4305425
Pauyoo <paulayodele04@gmail.com>
2026-01-16 01:18:27 +0100

    Counter

---
+++ index 2.js
@@ -0,0 +1,8 @@
+let fullname = "Atoyebi Paul";
+let newage = 25;
+let student = false;
+
+document.getElementById("p1").textContent = `Your name is ${fullname}`;
+document.getElementById("p1").textContent = `You are ${newage}`;
+document.getElementById("p1").textContent = `Enrolled: ${student}`;
+
---
+++ index.js
@@ -0,0 +1,140 @@
+//  
+console.log("Hello");
+console.log("My name is Paul");
+
+// window.alert("This is an alert");
+// window.alert("Hello");
+
+
+// document.getElementById("myhi").textContent="hello";
+// document.getElementById("myp").textContent="My name is paul";
+
+// Variables: declearation and assignment
+
+let age = 25;
+let networth = 1000;
+let gpa = 4.33;
+
+console.log("I am ${age} years old");
+console.log("My cgpa is ${gpa}")
+console.log("my current networth is $${networth}")
+// To print variable & data type
+let firstname = "paul";
+let email = "paulayodele04@gmail.com"
+
+console.log(typeof firstname);
+console.log(firstname);
+console.log(email);
+
+
+// Booleans 
+let online = false;
+let forsale = true;
+let availability = true;
+
+console.log(`paul is online: ${online}`);  //use backsticks for this
+console.log(`Is this car for sale: ${forsale}`);
+console.log(`product is available: ${availability}`)
+
+let fullname = "Atoyebi Paul";
+let newage = 25;
+let student = false;
+
+//* document.getElementById("p1").textContent = `Your name is ${fullname}`;
+// document.getElementById("p2").textContent = `You are ${newage}`;
+// document.getElementById("p3").textContent = `Enrolled: ${student}`;
+
+/// Arithmetic Operators 
+let waec_students = 20
+
+waec_students = waec_students + 1;
+waec_students = waec_students -2;
+waec_students = waec_students ** 2;  //raised to power 2
+// waec_students = waec_students % 2;  // modulus
+
+waec_students += 1;
+waec_students -= 1;
+waec_students /= 2;
+waec_students **=2;
+// waec_students %= 2; // modulus
+
+// increament and decreasement Operator 
+waec_students ++;
+waec_students --;
+
+console.log(waec_students);
+
+/*
+Operator Precedence
+1. Parenthesis()
+2. Exponents
+3. Multiply and add
+4. Add and subtract
+*/
+// Increament & Decreament Operator
+inc = 2;
+inc ++;
+inc --;
+
+console.log(inc);
+
+// Operator Operation
+let result = 1 + 2 * 3 + 4 ** 3;
+
+console.log(result);
+
+// Accepting User Input in Javascript
+// let username; 
+
+// username = window.prompt("What's your username");
+
+// console.log(username);
+
+// Accepting Inputs by creating HTML Textbox
+let username;
+
+// document.getElementById("mysubmit").onclick = function(){
+ //   username = document.getElementById("mytext").value;
+//    document.getElementById("myh1").textContent = `hello ${username}`
+// }
+
+// Changing data type & YTyper conversion
+// let experience = window.prompt("What is your years of experience?");
+//  experience = Number(experience);
+//  age+=1;
+
+//  console.log(experience, typeof experience);
+
+let a = "rice"
+let b = "bean"
+let c = "dodo"
+
+a = Number(a);
+b = String(b);
+c - Boolean(c);
+
+console.log(a, typeof a);
+console.log(b, typeof b);
+console.log(b, typeof b);
+
+
+/// CONSTANT & Math PI
+const PI = 3.14;
+let radius ;
+let circrumfrence;
+
+
+
+document.getElementById("mysubmit").onclick = function () {
+
+    let radius = document.getElementById("mytext").value;
+    radius = Number(radius);
+
+    let circumference = 2 * Math.PI * radius;
+
+    document.getElementById("myh3").textContent =
+        circumference.toFixed(2) + " cm";
+};
+
+
+
